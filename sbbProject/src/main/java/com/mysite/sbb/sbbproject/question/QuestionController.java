cefed6ff55faba37ea051ce2d22fa26561ac4cbc
rimrim990 <lim990718@naver.com>
2023-03-06 17:32:04 +0900

    add validator

--- sbbProject/src/main/java/com/mysite/sbb/sbbproject/question/QuestionController.java
+++ sbbProject/src/main/java/com/mysite/sbb/sbbproject/question/QuestionController.java
@@ -3,10 +3,13 @@ package com.mysite.sbb.sbbproject.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 // url prefix
@@ -33,5 +36,14 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        //this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";
     }
 }
