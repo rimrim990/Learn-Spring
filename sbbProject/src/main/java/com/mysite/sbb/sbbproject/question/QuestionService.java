5b5f31e7067525757418e6374b04f246bfa55cd8
rimrim990 <lim990718@naver.com>
2023-03-06 17:38:25 +0900

    add pagination

--- sbbProject/src/main/java/com/mysite/sbb/sbbproject/question/QuestionService.java
+++ sbbProject/src/main/java/com/mysite/sbb/sbbproject/question/QuestionService.java
@@ -2,6 +2,9 @@ package com.mysite.sbb.sbbproject.question;
 
 import com.mysite.sbb.sbbproject.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
@@ -14,8 +17,10 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
+    public Page<Question> getList(int page) {
+        // page - 조회할 페이지, size - 한 페이지 당 게시물의 수
+        Pageable pageable = PageRequest.of(page, 10);
+        return this.questionRepository.findAll(pageable);
     }
 
     public Question getQuestion(Integer id) {
