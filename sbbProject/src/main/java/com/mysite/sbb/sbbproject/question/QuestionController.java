75723398fe78f19dcc8fda4b61f240ddd96cfbed
rimrim990 <lim990718@naver.com>
2023-03-06 17:19:30 +0900

    add question detail page

--- sbbProject/src/main/java/com/mysite/sbb/sbbproject/question/QuestionController.java
+++ sbbProject/src/main/java/com/mysite/sbb/sbbproject/question/QuestionController.java
@@ -4,9 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+// url prefix
+@RequestMapping("/question")
 // final 이 붙은 속성을 포함하는 생성자를 자동으로 생성
 @RequiredArgsConstructor
 @Controller
@@ -14,7 +18,7 @@ public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
         // Model 객체는 자바 클래스와 템플릿 간의 연결고리 역할을 한다
@@ -22,5 +26,12 @@ public class QuestionController {
         // 컨트롤러 메서드의 매개변수로 지정하면 스프링부트가 자동으로 Model 객체를 생성한다
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- sbbProject/src/main/java/com/mysite/sbb/sbbproject/question/QuestionService.java
+++ sbbProject/src/main/java/com/mysite/sbb/sbbproject/question/QuestionService.java
@@ -1,9 +1,12 @@
 package com.mysite.sbb.sbbproject.question;
 
+import com.mysite.sbb.sbbproject.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -13,5 +16,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
