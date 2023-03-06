62d4b9972f7b537116f3521c690e05cc12ec3851
rimrim990 <lim990718@naver.com>
2023-03-06 16:31:33 +0900

    add entity repositories

---
+++ sbbProject/src/main/java/com/mysite/sbb/sbbproject/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.mysite.sbb.sbbproject;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
